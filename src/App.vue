36adfbe841a874ec01269ad0e3ed1167db17a9ba
Brian Zelip <bzelip@gmail.com>
2019-03-20 07:30:47 -0400

    App.vue: Register component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <main>
     <h1>Brian Zelip</h1>
     <div v-html="bio"></div>
+    <TheFooter></TheFooter>
   </main>
 </template>
 
 <script>
 import MarkdownIt from "markdown-it";
 
+import TheFooter from "./components/TheFooter.js";
 import Bio from "./content/bio.md";
 
 export default {
+  components: { TheFooter },
   computed: {
     bio() {
       const md = new MarkdownIt();
